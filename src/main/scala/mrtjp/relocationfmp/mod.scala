d279ccfdb1dd771d506fbe6253364a2f4544728d
glowredman <35727266+glowredman@users.noreply.github.com>
2022-12-17 23:39:12 +0100

    Update buildscript

--- src/main/scala/mrtjp/relocationfmp/mod.scala
+++ src/main/scala/mrtjp/relocationfmp/mod.scala
@@ -9,13 +9,12 @@ import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.event.{FMLInitializationEvent, FMLPostInitializationEvent, FMLPreInitializationEvent}
 import org.apache.logging.log4j.LogManager
 
-@Mod(modid = RelocationFMPMod.modID, useMetadata = true, modLanguage = "scala")
+@Mod(modid = RelocationFMPMod.modID, useMetadata = true, modLanguage = "scala", acceptedMinecraftVersions = "[1.7.10]", dependencies = "required-after:ForgeRelocation;required-after:ForgeMultipart", name = RelocationFMPMod.modName, version = RelocationFMPMod.version)
 object RelocationFMPMod
 {
     final val modID = "RelocationFMP"
     final val modName = "RelocationFMP"
-    final val version = "@VERSION@"
-    final val buildnumber = "@BUILD_NUMBER@"
+    final val version = "GRADLETOKEN_VERSION"
 
     val log = LogManager.getFormatterLogger(modID)
 
